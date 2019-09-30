9b61379311b2ebe45102a83c7ae94abb3ff6af37
isoppp <prog@d2deck.com>
2019-09-29 21:26:32 -0700

    add mocule basics

---
+++ module/app.ts
@@ -0,0 +1,6 @@
+import { PI, calculateCirumcumference } from './circle'
+import { calculateRectangle } from './rectangle'
+
+console.log(PI)
+console.log(calculateCirumcumference(3))
+console.log(calculateRectangle(1,2))
