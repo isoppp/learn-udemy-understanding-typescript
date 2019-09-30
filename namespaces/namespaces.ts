41badb3a616e37d169c00df8b856cba314bc0c14
isoppp <prog@d2deck.com>
2019-09-29 21:17:19 -0700

    add namespace basics

---
+++ namespaces/namespaces.ts
@@ -0,0 +1,7 @@
+/// <reference path="rectangleMath.ts" />
+/// <reference path="circleMath.ts" />
+
+console.log(MyMath.calculateRectangle(10,20))
+console.log(MyMath.Circle.calculateCircumference(3))
+
+// have to use tsc --outFile namespaces.js namespaces.ts
